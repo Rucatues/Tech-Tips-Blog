e43e0113ca2c6aae627bcd9de555ac4ec5048ed3
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-14 20:26:49 -0500

    added hooks for user email in user.js

--- models/user.js
+++ models/user.js
@@ -27,8 +27,24 @@ User.init(
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                len: [10],
+            },
         }
     }, {
+    hooks: {
+        // Use the beforeCreate hook to work with data before a new instance is created
+        beforeCreate: async (newUserData) => {
+            // taking the user's email address, and making all letters lower case before adding it to the database.
+            newUserData.email = await newUserData.email.toLowerCase();
+            return newUserData;
+        },
+        // make all of the characters lower case in an updated email address, before updating the database.
+        beforeUpdate: async (updatedUserData) => {
+            updatedUserData.email = await updatedUserData.email.toLowerCase();
+            return updatedUserData;
+        },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true, //uses singular table name instead of changing it to "Categorys"
