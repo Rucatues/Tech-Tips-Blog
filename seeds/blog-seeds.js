05a3824b092e024e5749155e18ef61364907df5d
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-14 20:34:54 -0500

    blog seeds filled in

---
+++ seeds/blog-seeds.js
@@ -0,0 +1,23 @@
+const { Blog } = require('../models/blog');
+
+const BlogData = [
+    {
+        title: 'Test Blog #1',
+        description: 'Test description #1',
+        user_id: 1
+    },
+    {
+        title: 'Test Blog #2',
+        description: 'Test description #2',
+        user_id: 2
+    },
+    {
+        title: 'Test Blog #3',
+        description: 'Test description #3',
+        user_id: 3
+    },
+];
+
+const seedBlogs = () => Blog.bulkCreate(BlogData);
+
+module.exports = seedBlogs; 
