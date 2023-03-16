1b4ad12640bfa4c27d0979ea13171816775b577a
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-16 08:21:58 -0500

    added outline of CRUD for each api route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,1 +1,28 @@
-const router = require('express').Router(); 
+const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.sendStatus(500).send(err);
+        console.log(err);
+    }
+});
+
+router.put('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.sendStatus(500).send(err);
+        console.log(err);
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.sendStatus(500).send(err);
+        console.log(err);
+    }
+}); 
