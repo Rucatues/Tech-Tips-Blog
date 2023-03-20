2155260071bd00b7431a0c05d33f1bef4907d5fb
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-19 21:07:17 -0500

    removed dashboard routes in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const blogRoutes = require('./blog-routes');
-// const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
 router.use('/blog', blogRoutes);
-// router.use('/comment', commentRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,18 +5,6 @@ const apiRoutes = require("./api")
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes)
-
-// router.get("/first-page", (req, res) => {
-//     res.render("page1")
-// })
-
-// router.get("/second-page", (req, res) => {
-//     res.render("page2", {
-//         layout: "dashboard",
-//         username: "john01"
-//     })
-// })
 
 
 module.exports = router;
