3d77b9b601374f479b21ac964c59d48687190854
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-03-20 18:35:02 -0500

    register route working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -56,9 +56,20 @@ router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/dashboard');
         return;
-    }
+    };
 
     res.render('login');
+
+});
+
+// takes user to sign up page
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup');
+    } catch {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 //takes user to dashboard page
@@ -82,15 +93,6 @@ router.get('/dashboard', Auth, async (req, res) => {
     }
 });
 
-//takes user to sign up page
-router.get('/signup', async (req, res) => {
-    try {
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
 
 //takes user to blog if they click on a certain blogpost
 router.get('/:blogid', async (req, res) => {
@@ -131,7 +133,6 @@ router.get('/dashboard/new', Auth, async (req, res) => {
         res.status(500).json(err);
     }
 });
-
 
 
 
